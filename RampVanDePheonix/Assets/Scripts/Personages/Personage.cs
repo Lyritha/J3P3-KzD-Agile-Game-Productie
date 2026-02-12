1a6f9bc4e14e5d72a0bed386741b94a200d17026
Jorick Wassink <wassinkjorick@gmail.com>
2026-02-12 14:35:49 +0100

    feat: party generator

---
+++ RampVanDePheonix/Assets/Scripts/Personages/Personage.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Personage", menuName = "Scriptable Objects/Personages")]
+public class Personage : ScriptableObject
+{
+    [Header("ID")]
+    public int id;
+
+    [Header("Name")]
+    public string name;
+
+    [Header("Vaardigheden")]
+    public int baseKapitaal;
+    public int baseBouwkunde;
+    public int baseLeervermogen;
+    public int baseSociaal;
+    public int baseAanpassingvermogen;
+
+    [Header("Lore")]
+    public string woonplaats;
+    public string beroep;
+    public string loreDrop;
+
+    [Header("Sprite")]
+    public Sprite portrait;
+}
