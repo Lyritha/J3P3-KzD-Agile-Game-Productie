a496d7ee906e1087aeadb71b7f6e7b16450e6bf3
Jorick Wassink <wassinkjorick@gmail.com>
2026-02-20 13:31:51 +0100

    feat: changed bug about the party and a few UI changes

--- RampVanDePheonix/Assets/Scripts/Personages/PartyGen.cs
+++ RampVanDePheonix/Assets/Scripts/Personages/PartyGen.cs
@@ -1,19 +1,36 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PartyGen : MonoBehaviour
 {
+    CharacterListDisplay characterList;
+    Personage[] personages;
     public List<Personage> party = new List<Personage>();
     void Start()
     {
-        Personage[] personages = Resources.LoadAll<Personage>("Personages");
+        personages = Resources.LoadAll<Personage>("Personages");
+        characterList = FindAnyObjectByType<CharacterListDisplay>();
 
         do
         {
             int randomNum = Random.Range(0, personages.Length);
-            if (!party.Contains(personages[randomNum])) party.Add(Instantiate(personages[randomNum]));
+            if (!CompareCharacter(randomNum)) party.Add(Instantiate(personages[randomNum]));
+            else continue;
             //if (!party.Contains(personages[randomNum])) party.Add(new Personage(personages[randomNum].id, personages[randomNum].name, personages[randomNum].baseKapitaal, personages[randomNum].baseBouwkunde, personages[randomNum].baseLeervermogen, personages[randomNum].baseSociaal, personages[randomNum].baseAanpassingsvermogen, personages[randomNum].woonplaats, personages[randomNum].beroep, personages[randomNum].loreDrop, personages[randomNum].portrait));
 
         } while (party.Count < 4);
+
+        characterList.AddCharacters(party);
+    }
+
+
+    bool CompareCharacter(int randomNum)
+    {
+        foreach(Personage personage in party)
+        {
+            if(personage.id == personages[randomNum].id) return true;
+        }
+        return false;
     }
 }
