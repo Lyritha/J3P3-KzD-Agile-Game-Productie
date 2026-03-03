33c16f867060ec8924e332d169498ffa3f1597f0
THERMELINK <thermelink01@student.rocvantwente.nl>
2026-03-03 11:48:22 +0100

    feat: made a beginning for the lifeboat scene thing
    something like that

---
+++ RampVanDePheonix/Assets/Scripts/LifeboatScripts/LifeboatManager.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeboatManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    List<GameObject> LifeBoatPassengers;
+    [SerializeField] GameObject BoatSeatsPlacement;
+    GameObject Boat;
+    void Start()
+    {
+        Boat = gameObject;
+        LifeBoatPassengers = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChoosePersonToBeSaved(GameObject character)
+    {
+        LifeBoatPassengers.Add(character);
+        SetIsSafeStatus(character);
+    }
+    void SetIsSafeStatus(GameObject character)
+    {
+        //get character script and set it to "is safe" (need to change the character script)
+    }
+}
