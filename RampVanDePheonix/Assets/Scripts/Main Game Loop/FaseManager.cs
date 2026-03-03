488ffaecbaa17b4c4b3b2b8e0de26d98a8dc07bd
Jorick Wassink <wassinkjorick@gmail.com>
2026-03-03 09:14:26 +0100

    feat: fases now change when method is called
    now still works with a placeholder piece of code but can be edited easily

---
+++ RampVanDePheonix/Assets/Scripts/Main Game Loop/FaseManager.cs
@@ -0,0 +1,100 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+public enum Fases
+{
+    Achterhoek,
+    Pheonix,
+    Amerika,
+    EndScreen
+}
+public class FaseManager : MonoBehaviour
+{
+    [Header("Pre-Filled Lists")]
+    public List<QuestionScriptable> achterhoekEvents = new List<QuestionScriptable>();
+    public List<QuestionScriptable> pheonixEvents = new List<QuestionScriptable>();
+    public List<QuestionScriptable> amerikaEvents = new List<QuestionScriptable>();
+
+
+    [Header("Current Events (dont touch)")]
+    public List<QuestionScriptable> currentEvents = new List<QuestionScriptable>();
+
+    [Header("WinScreenPrefab")]
+    [SerializeField] GameObject winScreen;
+
+    PhaseDisplayer display;
+    Fases currentFase;
+    int progress = 0;
+    int maxProgress = 10;
+    private void Start()
+    {
+        display = FindAnyObjectByType<PhaseDisplayer>();
+
+        SwapFase(Fases.Achterhoek); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            AddProgress();
+            print("progress =  " + progress);
+        }
+    }
+
+    void SwapFase(Fases fase)
+    {
+        currentFase = fase; 
+        switch (currentFase)
+        {
+            case Fases.Achterhoek:
+                FillList(achterhoekEvents);
+                break;
+            case Fases.Pheonix:
+                FillList(pheonixEvents);
+                break;
+            case Fases.Amerika:
+                FillList(amerikaEvents);
+                break;
+            case Fases.EndScreen:
+                Instantiate(winScreen);
+                break;
+        }
+        display.SetPhase(currentFase,10);
+    }
+
+    void FillList(List<QuestionScriptable> incomingEvents)
+    {
+        currentEvents.Clear();
+        foreach(QuestionScriptable p in incomingEvents)
+        {
+            currentEvents.Add(p);
+        }
+    }
+
+    public void AddProgress()
+    {
+        progress++;
+
+        if (progress >= maxProgress)
+        {
+            display.SetPhase(GetNextPhase(currentFase), 10);
+            SwapFase(GetNextPhase(currentFase));
+
+            progress = 0;
+        }
+
+        display.IncrementProgress();
+        //UPDATE UI
+    }
+
+    Fases GetNextPhase(Fases currentPhase)
+    {
+        if (currentPhase == Fases.Achterhoek) return Fases.Pheonix;
+        if(currentPhase == Fases.Pheonix) return Fases.Amerika;
+        if (currentFase == Fases.Amerika) return Fases.EndScreen;
+        return Fases.Achterhoek;
+    }
+
+}
