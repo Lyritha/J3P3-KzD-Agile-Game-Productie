605bdda7b81703ece33c7643d5979a565f40575c
Jorick Wassink <wassinkjorick@gmail.com>
2026-03-31 08:41:20 +0100

    feat: fixed shop appearing in boat

--- RampVanDePheonix/Assets/Scripts/Main Game Loop/EventStateManager.cs
+++ RampVanDePheonix/Assets/Scripts/Main Game Loop/EventStateManager.cs
@@ -116,7 +116,7 @@ public class EventStateManager : MonoBehaviour
                 StartCoroutine(WalkingState(walkTimeSecs));
                 break;
             case State.Shop:
-                ShopState(); 
+                if(faseManager.CurrentFase != Fases.Pheonix) ShopState(); 
                 break;
             case State.Event:
                 EventState();
